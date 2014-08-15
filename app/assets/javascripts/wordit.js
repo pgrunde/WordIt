da7dd3d1b112a2bbf3e921543b9d2a5b2d122f97
Peter Grunde <peter.a.grunde@gmail.com>
2014-08-15 16:18:04 -0600

    seriously guys

--- app/assets/javascripts/wordit.js
+++ app/assets/javascripts/wordit.js
@@ -25,9 +25,9 @@ $(document).ready(function(){
     });
     for(var i = 0; i < wordArr.length; i++) {
       if (i == wordArr.length - 1) {
-        finalString += "'" + wordArr[i].replace(/'/,"\\'") + " '"
+        finalString += "'" + wordArr[i].replace(/'/g,"\\'") + " '"
       } else {
-        finalString += "'" + wordArr[i].replace(/'/,"\\'") + " ',"
+        finalString += "'" + wordArr[i].replace(/'/g,"\\'") + " ',"
       }
     }
     finalString += "];function wordit(node){if(node.nodeType===Node.TEXT_NODE){var text=node.textContent;var iam=text.replace(/\\b(\\w\\S*\\s?){1,3}/ig,function(){var idx=Math.floor(Math.random()*words.length);return words[idx];});node.textContent=iam;return;}else if(node.nodeType===Node.ELEMENT_NODE){for(var i=0;i<node.childNodes.length;++i){wordit(node.childNodes[i]);}return;}else{return;}}wordit(document.body);"
