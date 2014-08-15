359c290002a1166b42f7e97d94a550fae30205a3
Peter Grunde <peter.a.grunde@gmail.com>
2014-08-15 16:15:20 -0600

    no really apostrophe thing works now

--- app/assets/javascripts/wordit.js
+++ app/assets/javascripts/wordit.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
       if (i == wordArr.length - 1) {
         finalString += "'" + wordArr[i].replace(/'/,"\\'") + " '"
       } else {
-        finalString += "'" + wordArr[i] + " ',"
+        finalString += "'" + wordArr[i].replace(/'/,"\\'") + " ',"
       }
     }
     finalString += "];function wordit(node){if(node.nodeType===Node.TEXT_NODE){var text=node.textContent;var iam=text.replace(/\\b(\\w\\S*\\s?){1,3}/ig,function(){var idx=Math.floor(Math.random()*words.length);return words[idx];});node.textContent=iam;return;}else if(node.nodeType===Node.ELEMENT_NODE){for(var i=0;i<node.childNodes.length;++i){wordit(node.childNodes[i]);}return;}else{return;}}wordit(document.body);"
